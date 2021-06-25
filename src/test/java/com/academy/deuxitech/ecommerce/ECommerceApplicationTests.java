f393c28c81e6e09181e30219b60488eb24e887d1
ValMasse <valmasse@gmx.fr>
2021-06-25 20:48:51 +0200

    first commit

---
+++ src/test/java/com/academy/deuxitech/ecommerce/ECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academy.deuxitech.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
